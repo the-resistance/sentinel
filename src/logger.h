998fb3e2fcc262c270b8ad14ceb74d09493fa8ea
the-resistance <trixboxx@gmail.com>
2025-08-01 17:36:49 -0700

    Update logger.h

--- src/logger.h
+++ src/logger.h
@@ -1,12 +1,11 @@
-// logger.h
-// Signal logging to CSV and SQLite
 #ifndef LOGGER_H
 #define LOGGER_H
 
 #include <stdint.h>
+#include <stdio.h>
 
 void logger_init(const char *csv_path, const char *sqlite_path);
-void logger_record(uint64_t freq_hz, int rssi, int status);
+void logger_record(uint64_t freq_hz, int rssi, const char *status_str);
 void logger_close(void);
 
 #endif
