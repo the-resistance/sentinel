b65be26daa0b93f1ef45dec1325d392ae05ab8da
the-resistance <trixboxx@gmail.com>
2025-08-02 22:59:09 +0000

    Update sweep.h

--- src/sweep.h
+++ src/sweep.h
@@ -1,10 +1,19 @@
-#ifndef SCANNER_H
-#define SCANNER_H
+#ifndef SWEEP_H
+#define SWEEP_H
 
 #include <stdint.h>
 
-void init_scanner();
-void perform_scan(uint64_t start_freq, uint64_t end_freq, uint32_t step_hz);
-void shutdown_scanner();
+// Sweep configuration
+typedef struct {
+    uint64_t start_freq_hz;
+    uint64_t end_freq_hz;
+    uint32_t step_hz;
+    uint32_t dwell_ms;
+} sweep_config_t;
 
-#endif // SCANNER_H
+void sweep_init(sweep_config_t *config);
+uint64_t sweep_next_frequency();
+int sweep_has_next();
+void sweep_reset();
+
+#endif // SWEEP_H
