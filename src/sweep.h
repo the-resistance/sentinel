a69c135e0b30d055320624978717e8aa9b16b07a
the-resistance <trixboxx@gmail.com>
2025-08-01 17:55:51 -0700

    Update sweep.h

--- src/sweep.h
+++ src/sweep.h
@@ -1,25 +1,16 @@
-// sweep.h
-// RF sweep engine interface for rf-sentinel
 #ifndef SWEEP_H
 #define SWEEP_H
 
 #include <stdint.h>
 
-typedef enum {
-    MODE_FULL,
-    MODE_TACTICAL
-} sweep_mode_t;
-
 typedef struct {
-    uint64_t freq_start_hz;
-    uint64_t freq_end_hz;
+    uint64_t start_freq;
+    uint64_t end_freq;
     uint64_t step_hz;
-    int dwell_ms;
-    int rssi_threshold;
     uint32_t sample_rate;
-    sweep_mode_t mode;
+    uint32_t dwell_ms;
 } sweep_config_t;
 
-int sweep_run(const sweep_config_t *cfg, volatile int *keep_running);
+int run_sweep(const sweep_config_t *cfg);
 
 #endif
