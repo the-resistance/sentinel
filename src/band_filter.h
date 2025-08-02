7f2cafdaa4693fce64ff0c4f0a992ab2a47e2f13
the-resistance <trixboxx@gmail.com>
2025-08-01 17:44:29 -0700

    Update band_filter.h

--- src/band_filter.h
+++ src/band_filter.h
@@ -1,19 +1,16 @@
-// band_filter.h
-// Frequency suppression and tactical range filters
 #ifndef BAND_FILTER_H
 #define BAND_FILTER_H
 
 #include <stdint.h>
 
-#define MAX_FILTERS 64
+#define MAX_BANDS 128
 
 typedef struct {
     uint64_t start;
     uint64_t end;
 } band_range_t;
 
-int load_band_filters(const char *ignore_file, const char *tactical_file);
-int band_should_ignore(uint64_t freq_hz);
-int band_is_tactical(uint64_t freq_hz);
+int load_bands(const char *filename, band_range_t *bands, int max);
+int in_any_band(uint64_t freq, const band_range_t *bands, int count);
 
 #endif
