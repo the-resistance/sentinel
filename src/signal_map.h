91bc6047f64d9a72334f2c36e296300d9a335e07
the-resistance <trixboxx@gmail.com>
2025-08-03 01:28:21 +0000

    Update signal_map.h

--- src/signal_map.h
+++ src/signal_map.h
@@ -1,21 +1,9 @@
 // signal_map.h
-// Runtime signal state tracking (in RAM)
+// version: v1.3.1
+
 #ifndef SIGNAL_MAP_H
 #define SIGNAL_MAP_H
 
-#include <stdint.h>
-
-#define MAX_SIGNALS 2048
-
-typedef struct {
-    uint64_t freq;
-    int rssi;
-    int hits;
-    uint64_t first_seen;
-    uint64_t last_seen;
-} signal_entry_t;
-
-void signal_map_init(void);
-int signal_map_register(uint64_t freq, int rssi);
+const char* resolve_signal(uint64_t freq);
 
 #endif
