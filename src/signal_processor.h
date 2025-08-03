7835da26ce16b7473771565f6e262a701a290d98
the-resistance <trixboxx@gmail.com>
2025-08-03 01:27:34 +0000

    Create signal_processor.h

---
+++ src/signal_processor.h
@@ -0,0 +1,11 @@
+// signal_processor.h
+// version: v1.3.1
+
+#ifndef SIGNAL_PROCESSOR_H
+#define SIGNAL_PROCESSOR_H
+
+#include <stdint.h>
+
+int analyze_rssi(uint64_t freq, double rssi);
+
+#endif
