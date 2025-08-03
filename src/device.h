6c60cd94680f8aa9970d069f9d4c84c26b7a8eb9
the-resistance <trixboxx@gmail.com>
2025-08-03 01:29:12 +0000

    Update device.h

--- src/device.h
+++ src/device.h
@@ -1,16 +1,26 @@
+// device.h
+// version: v1.3.1
+
 #ifndef DEVICE_H
 #define DEVICE_H
 
 #include <stdint.h>
 
 typedef struct {
-    char label[64];
-    uint64_t freq;
-    int rssi;
-    int score;
-} DeviceInfo;
+    int device_index;
+    uint64_t start_freq;
+    uint64_t end_freq;
+    uint64_t step_hz;
+    int dwell_ms;
+    int signal_threshold;
+} config_t;
 
-void identify_device(DeviceInfo *dev);
-const char* resolve_vendor(uint64_t freq);
+typedef struct {
+    void *handle;
+} device_t;
+
+int parse_args(int argc, char **argv, config_t *config);
+int init_device(device_t *dev, int index);
+double measure_rssi(device_t *dev, uint64_t freq, int dwell_ms);
 
 #endif
