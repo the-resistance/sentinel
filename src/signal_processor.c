d085c51bdc7ad8c1d8ca8f9ec7b5a5494b96930e
the-resistance <trixboxx@gmail.com>
2025-08-04 12:00:58 +0000

    Update signal_processor.c

--- src/signal_processor.c
+++ src/signal_processor.c
@@ -1,50 +1,42 @@
 // signal_processor.c — Scan Routines
 // Version: 1.0.4
 
-#define _POSIX_C_SOURCE 200809L
-
+#define _XOPEN_SOURCE 700
+#include <unistd.h>   // usleep()
 #include <stdio.h>
-#include <stdlib.h>     // rand()
-#include <unistd.h>     // usleep()
+#include <stdlib.h>
 #include "device.h"
-#include "signal_processor.h"
 #include "logger.h"
+#include "signal_processor.h"
 #include "band_filter.h"
 #include "config.h"
 
 void run_quick_scan(device_t *dev) {
-    for (int freq = 900000000; freq < 915000000; freq += 100000) {
+    for (float freq = 100.0; freq < 200.0; freq += 0.5) {
         int rssi = rand() % 100;
-
         if (rssi >= DEFAULT_RSSI_THRESHOLD) {
             log_event("QuickScan match", freq, rssi);
         }
-
         usleep(QUICKSCAN_DWELL_MS * 1000);
     }
 }
 
 void run_full_scan(device_t *dev) {
-    for (int freq = 900000000; freq < 915000000; freq += 50000) {
+    for (float freq = 100.0; freq < 200.0; freq += 0.1) {
         int rssi = rand() % 100;
-
         if (rssi >= DEFAULT_RSSI_THRESHOLD) {
-            log_event("FullScan hit", freq, rssi);
+            log_event("FullScan match", freq, rssi);
         }
-
         usleep(FULLSCAN_DWELL_MS * 1000);
     }
 }
 
 void run_protocol_scan(device_t *dev) {
-    int freqs[] = {935000000, 940000000, 950000000, 960000000};
-    for (int i = 0; i < 4; ++i) {
+    for (float freq = 100.0; freq < 200.0; freq += 0.2) {
         int rssi = rand() % 100;
-
         if (rssi >= DEFAULT_RSSI_THRESHOLD) {
-            log_event("ProtocolScan hit", freqs[i], rssi);
+            log_event("Protocol match", freq, rssi);
         }
-
         usleep(PROTOCOLSCAN_DWELL_MS * 1000);
     }
 }
